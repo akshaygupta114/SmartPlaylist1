62665eb18b9069494fc3282f861aad9e83947ae2
Akshay Gupta <akshaygupta1109@gmail.com>
2014-02-27 02:08:50 -0500

    Login With Facebook Button, All Settings app

---
+++ src/com/example/smartplaylist1/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.smartplaylist1;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import com.facebook.*;
+import com.facebook.model.*;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
