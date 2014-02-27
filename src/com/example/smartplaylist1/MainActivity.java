54fc59d44ae46ce44828fb53bd259b50fc0284f4
Thiaga <thiagarajan.ts@gmail.com>
2014-02-27 16:55:53 -0500

    Removed unneeded includes

--- src/com/example/smartplaylist1/MainActivity.java
+++ src/com/example/smartplaylist1/MainActivity.java
@@ -2,10 +2,7 @@ package com.example.smartplaylist1;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.View;
-import android.view.ViewGroup;
 
 public class MainActivity extends FragmentActivity {
 
